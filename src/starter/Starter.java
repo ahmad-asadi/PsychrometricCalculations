8d0943c46838cd5dafdc153a4fca7ff00076338a
ahmad-asadi <ahmad.asadi.ir@gmail.com>
2017-01-10 20:41:44 +0330

    project created

---
+++ src/starter/Starter.java
@@ -0,0 +1,19 @@
+package starter;
+
+import javax.swing.*;
+
+/**
+ * This class is created by Ahmad Asadi on 1/10/17.
+ */
+public class Starter {
+
+    private Starter(){
+        JFrame mainFrame = new JFrame() ;
+        mainFrame.setSize(SystemTo);
+    }
+
+    public static void main(String[] args)
+    {
+        new Starter() ;
+    }
+}
