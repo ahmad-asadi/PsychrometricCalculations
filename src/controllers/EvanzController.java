85417d394c1e8036a2607691e775249ab5e174ff
Ahmad <ahmad.asadi.ir@gmail.com>
2017-01-26 00:42:06 +0330

    Evanz index has been added

--- src/controllers/EvanzController.java
+++ src/controllers/EvanzController.java
@@ -1,7 +1,6 @@
 package controllers;
 
 import javax.swing.table.DefaultTableModel;
-import java.util.Vector;
 
 /**
  * This class is created by Ahmad Asadi on 1/17/17.
@@ -12,17 +11,14 @@ public class EvanzController extends IndexController{
         super();
         setCols();
         numberOfVars = 4 ;
-        numberOfRes = 12 ;
-        indexOfStringField = 10 ;
-        bounds = new double[]{90,105,130,Double.MAX_VALUE} ;
-        boundStrings = new String[]{"احتیاط","احتیاط بسیار","خطرناک","بسیار خطرناک"} ;
+        numberOfRes = 18 ;
+        indexOfStringField = 16 ;
         setBoundStrings = true ;
     }
 
     @Override
     protected double computeRes(double[] inputs, int index){
-        double [] bounds = new double[]{30,50,70,100,Double.MAX_VALUE} ;
-        double [][] vals = new double[][]{
+        double [][] maxVals = new double[][]{
                 {32.5,30.5,29.5,29,Double.MAX_VALUE},
                 {30,28,27.5,27,Double.MAX_VALUE},
                 {22.5,22.5,22.5,22.5,Double.MAX_VALUE},
@@ -30,23 +26,77 @@ public class EvanzController extends IndexController{
                 {27.5,26.5,26,25.5,Double.MAX_VALUE},
                 {20,20,20,20,Double.MAX_VALUE},
         };
-        double input ;
+        double [][] minVals = new double[][]{
+                {29.5,28.5,27.5,26,Double.MAX_VALUE},
+                {22.5,22.5,22.5,22.5,Double.MAX_VALUE},
+                {18,18,18,18,Double.MAX_VALUE},
+                {27.5,26.5,26,25.5,Double.MAX_VALUE},
+                {20,20,20,20,Double.MAX_VALUE},
+                {16,16,16,16,Double.MAX_VALUE},
+        };
 
-        if(index < 3 || (index >= 6 && index < 9))
-            input = inputs[3] ;
+        double[][] vals ;
+        if(index % 2 == 0)
+            vals = maxVals ;
+        else
+            vals = minVals ;
+
+        return matchColumn(inputs,index,vals) ;
+    }
+
+    private Double matchColumn(double[] inputs, int index, double[][] vals) {
+        double [] bounds = new double[]{30,50,70,100,Double.MAX_VALUE} ;
+        double input;
+        if (index < 6 || (index > 11 && index < 18))
+            input = inputs[3];
         else
             input = inputs[2] ;
-
+        index %= 12 ;
         for(int i = 0 ; i < bounds.length ; i++)
             if(input < bounds[i])
-                return vals[index%6][i] ;
-        return  Double.MAX_VALUE ;
+                return vals[index/2][i] ;
+        return Double.MAX_VALUE;
     }
 
     @Override
     protected void setBoundStrings(double[] res)
     {
+        model = (DefaultTableModel) getModel();
+        for(int stringIndex = 0 ; stringIndex < 6 ; stringIndex++){
 
+            for(int j = 1 ; j < cols.length ; j++) {
+                double maxValue ;
+                try {
+                    maxValue = Double.parseDouble((String) model.getValueAt(numberOfVars + stringIndex * 2, j));
+                }catch (Exception e)
+                {
+                    maxValue = 0 ;
+                }
+                double minValue ;
+                try {
+                    minValue = Double.parseDouble((String) model.getValueAt(numberOfVars + stringIndex * 2 + 1, j));
+                }catch (Exception e)
+                {
+                    minValue = 0 ;
+                }
+                int inputIndex = 0 ;
+                if(stringIndex < 3)
+                    inputIndex = 1 ;
+                double temper ;
+                try {
+                    temper = Double.parseDouble((String) model.getValueAt(inputIndex, j));
+                }catch (Exception e)
+                {
+                    temper = 0 ;
+                }
+                if(temper > maxValue)
+                    model.setValueAt("گرم" , stringIndex + indexOfStringField , j);
+                else if(temper < minValue)
+                    model.setValueAt("سرد" , stringIndex + indexOfStringField , j);
+                else
+                    model.setValueAt("راحت" , stringIndex + indexOfStringField , j);
+            }
+        }
     }
 
     @Override
@@ -62,17 +112,30 @@ public class EvanzController extends IndexController{
         row[0] = "میانگین حداقل رطوبت نسبی" ;
         model.addRow(row);
 
-        row[0] = "مقیاس الف منطقه راحت روز" ;
+        row[0] = "بیشینه مقیاس الف منطقه راحت روز" ;
         model.addRow(row);
-        row[0] = "مقیاس ب منطقه راحت روز" ;
+        row[0] = "کمینه مقیاس الف منطقه راحت روز" ;
         model.addRow(row);
-        row[0] = "مقیاس ج منطقه راحت روز" ;
+        row[0] = "بیشینه مقیاس ب منطقه راحت روز" ;
         model.addRow(row);
-        row[0] = "مقیاس الف منطقه راحت شب" ;
+        row[0] = "کمینه مقیاس ب منطقه راحت روز" ;
         model.addRow(row);
-        row[0] = "مقیاس ب منطقه راحت شب" ;
+        row[0] = "بیشینه مقیاس ج منطقه راحت روز" ;
         model.addRow(row);
-        row[0] = "مقیاس ج منطقه راحت شب" ;
+        row[0] = "کمینه مقیاس ج منطقه راحت روز" ;
+        model.addRow(row);
+
+        row[0] = "بیشینه مقیاس الف منطقه راحت شب" ;
+        model.addRow(row);
+        row[0] = "کمینه مقیاس الف منطقه راحت شب" ;
+        model.addRow(row);
+        row[0] = "بیشینه مقیاس ب منطقه راحت شب" ;
+        model.addRow(row);
+        row[0] = "کمینه مقیاس ب منطقه راحت شب" ;
+        model.addRow(row);
+        row[0] = "بیشینه مقیاس ج منطقه راحت شب" ;
+        model.addRow(row);
+        row[0] = "کمینه مقیاس ج منطقه راحت شب" ;
         model.addRow(row);
 
         row[0] = "مقیاس الف منطقه راحت روز" ;
