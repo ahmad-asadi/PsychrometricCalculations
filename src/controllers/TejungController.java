5f9d5dbf65146fee0d7f11f6e0a6bd55746c9999
root <ahmad.asadi.ir@gmail.com>
2017-02-26 23:48:58 +0330

    synch

--- src/controllers/TejungController.java
+++ src/controllers/TejungController.java
@@ -4,6 +4,7 @@ import uiElements.ThermalChartFrame;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
+import java.awt.*;
 import java.util.ArrayList;
 
 /**
@@ -11,21 +12,100 @@ import java.util.ArrayList;
  */
 public class TejungController extends IndexController {
 
+    private final JTable table1;
+    private final JTable table2;
+    private final JTable table3;
+    private final JTable table4;
+    private final JTabbedPane tabbedPane;
+
     private double lat = 0 ;
 
     public TejungController(){
         super();
         numberOfVars = 9 ;
-        numberOfRes = 16 ;
+        numberOfRes = 15 ;
         System.out.println(getResList().length);
         indexOfBoundStrings = new ArrayList<>();
+        indexOfBoundStrings.add(11);
         indexOfBoundStrings.add(12);
         indexOfBoundStrings.add(13);
         indexOfBoundStrings.add(14);
-        indexOfBoundStrings.add(15);
         resTable.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
         varTable.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+
+
+        String[] cols1 = new String[]{"ردیف","ضریب تاثیر باد در روز"} ;
+        String[] cols3 = new String[]{"ردیف","محاسبه دفع انرژی در شب","سمبل شب","احساس غالب"} ;
+        String[] cols4 = new String[]{"ردیف","محاسبه دفع انرژی در روز","سمبل روز","احساس غالب"} ;
+        String[] cols5 = new String[]{"ردیف","سمبل شب","احساس غالب در شب","سمبل روز","احساس غالب در روز"} ;
+
+        table1 = new JTable(new String[400][cols1.length],cols1) ;
+        table2 = new JTable(new String[400][cols3.length],cols3) ;
+        table3 = new JTable(new String[400][cols4.length],cols4) ;
+        table4 = new JTable(new String[400][cols5.length],cols5) ;
+
+
+        JScrollPane jsp1 = createNewSubTable(table1);
+        JScrollPane jsp2 = createNewSubTable(table2);
+        JScrollPane jsp3 = createNewSubTable(table3);
+        JScrollPane jsp4 = createNewSubTable(table4);
+
+
+        tabbedPane = new JTabbedPane() ;
+
+        tabbedPane.setSize(resJsp.getSize());
+        tabbedPane.add("ضریب تاثیر باد در روز",jsp1) ;
+        tabbedPane.add("دفع انرژی در شب",jsp2) ;
+        tabbedPane.add("دفع انرژی در روز",jsp3) ;
+        tabbedPane.add("نتیجه نهایی",jsp4) ;
+        tabbedPane.setLocation(resJsp.getLocation());
+        tabbedPane.setBorder(BorderFactory.createBevelBorder(0));
+        tabbedPane.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
+
+        remove(resJsp);
+        add(tabbedPane) ;
+
     }
+
+    private JScrollPane createNewSubTable(JTable table1) {
+        JScrollPane jsp1 = new JScrollPane(table1) ;
+        jsp1.setSize(table1.getSize());
+        jsp1.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
+        table1.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+        table1.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
+
+        table1.getColumnModel().getColumn(0).setPreferredWidth(100);
+        for(int i = 1 ; i < table1.getColumnCount() ; i++)
+            table1.getColumnModel().getColumn(i).setPreferredWidth(180);
+
+        table1.setFillsViewportHeight(true);
+        return jsp1;
+    }
+
+    @Override
+    public void solve(){
+        super.solve();
+        for(int j = 0 ; j < resTable.getRowCount() ; j++) {
+            for (int i = 0; i < 2; i++) {
+                table1.setValueAt(resTable.getValueAt(j, i), j, i);
+            }
+            table2.setValueAt(resTable.getValueAt(j,0),j,0);
+            for (int i = 2; i < 5; i++) {
+                table2.setValueAt(resTable.getValueAt(j, i), j, i-1);
+            }
+            table3.setValueAt(resTable.getValueAt(j,0),j,0);
+            for (int i = 5; i < 8; i++) {
+                table3.setValueAt(resTable.getValueAt(j, i), j, i-4);
+            }
+            table4.setValueAt(resTable.getValueAt(j,0),j,0);
+            for (int i = 8; i < 12; i++) {
+                table4.setValueAt(resTable.getValueAt(j, i), j, i-7);
+            }
+        }
+
+    }
+
+
 
     @Override
     protected String[] getVarList() {
@@ -34,7 +114,7 @@ public class TejungController extends IndexController {
 
     @Override
     protected String[] getResList() {
-        return new String[]{"میانگین ساعت واقعی آفتاب روزانه","مدار میل خورشید","میانگین سرعت باد به متر بر ثانیه","WS(+24)","WS(-24)","طول روز","ضریب تاثیر باد در روز","میزان دفع انرژی شبانه","انرژی خورشیدی روزانه","قدرت خنک\u200Cکنندگی خالص باد در روز","منفی‌دار کردن پاسخ","میزان دفع انرژی روزانه","سمبل روز","سمبل شب","احساس غالب روز","احساس غالب شب"};
+        return new String[]{"مدار میل خورشید","میانگین سرعت باد به متر بر ثانیه","WS(+24)","WS(-24)","طول روز","ضریب تاثیر باد در روز","میزان دفع انرژی شبانه","انرژی خورشیدی روزانه","قدرت خنک\u200Cکنندگی خالص باد در روز","منفی‌دار کردن پاسخ","میزان دفع انرژی روزانه","سمبل روز","سمبل شب","احساس غالب روز","احساس غالب شب"};
     }
 
     @Override
@@ -77,29 +157,29 @@ public class TejungController extends IndexController {
 
 
         switch (index){
+//            case 0:
+//                return B6 ;
             case 0:
-                return B6 ;
-            case 1:
                 return R4 ;
-            case 2:
+            case 1:
                 return C23 ;
-            case 3:
+            case 2:
                 return C25 ;
-            case 4:
+            case 3:
                 return C27 ;
-            case 5 :
+            case 4 :
                 return C29;
-            case 6:
+            case 5:
                 return D7 ;
-            case 7:
+            case 6:
                 return D77 ;
-            case 8:
+            case 7:
                 return B9 ;
-            case 9:
+            case 8:
                 return B10 ;
-            case 10:
+            case 9:
                 return B11 ;
-            case 11:
+            case 10:
                 return B12 ;
             default:
                 return -1 ;
